79fb857a4199b5a4415f178279fa24d2b0555747
David Preiss <dap152@pitt.edu>
2021-04-21 12:03:44 -0400

    first commit

---
+++ src/main/java/MainRunner.java
@@ -0,0 +1,17 @@
+public class MainRunner
+{
+    public static void main(String[] args)
+    {
+        //start main
+        System.out.println("\t Main Start \n");
+
+        /*
+        read the book and count how many time each word appeared
+ 1. choose a byte or char base (reader/ input) to read the book
+ 2. read into a collection and sort
+ 3. clean the data ... e.g. (- , extra spaces , upper / lower )
+ 4. display the data on the console
+         */
+    }
+    //end main
+}
